2741bc24b2f5e552e06be1982f2ad6160cd450a5
Nithin S <nithinselvam333@gmail.com>
2025-09-06 13:17:16 +0530

    Add files via upload

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,5 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
+R.c("server/chunks/ssr/[root-of-the-server]__ff74d25b._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__e9d39045._.js")
+R.m(29512)
+module.exports=R.m(29512).exports
---
+++ .next/server/pages/_error.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
+R.c("server/chunks/ssr/[root-of-the-server]__ff74d25b._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__e9d39045._.js")
+R.c("server/chunks/ssr/7e54a_next_1cf8b917._.js")
+R.c("server/chunks/ssr/[externals]_next_dist_shared_lib_no-fallback-error_external_59b92b38.js")
+R.c("server/chunks/ssr/7e54a_b8943040._.js")
+R.m(80413)
+module.exports=R.m(80413).exports
