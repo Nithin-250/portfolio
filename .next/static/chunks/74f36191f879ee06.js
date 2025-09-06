7ba3ebb284723e1fde05a259b0b1fbbccce312fb
Nithin S <nithinselvam333@gmail.com>
2025-09-06 13:18:18 +0530

    Add files via upload

---
+++ .next/static/chunks/74f36191f879ee06.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/5ca6a8585d8b72f0.js",
+  "static/chunks/9c37181e896a7f8b.js",
+  "static/chunks/turbopack-841a697453b40edb.js"
+])
---
+++ .next/static/chunks/c47dd6fd61e3891c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/94d635b4c0cdab9f.js",
+  "static/chunks/9c37181e896a7f8b.js",
+  "static/chunks/turbopack-61a8fe11e015f07c.js"
+])
